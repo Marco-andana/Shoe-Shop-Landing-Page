187bc8bf9b2dd027b9f7674967d97de4d4d16d2b
Marco-andana <110299791+Marco-andana@users.noreply.github.com>
2022-10-10 22:45:44 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+console.log('starting tweak');
+const product1 = document.getElementById("product1");
+const wrapper = document.getElementById("wrapper");
+const newBox = document.getElementById("newBox");
+const sale = document.getElementById("sale");
+const cta = document.querySelector('.cta');
+const productBox1 = document.getElementById("productBox1");
+const productBox2 = document.getElementById("productBox2");
+const productBox3 = document.getElementById("productBox3");
+const productBox4 = document.getElementById("productBox4");
+const productBox5 = document.getElementById("productBox5");
+const productBox6 = document.getElementById("productBox6");
+
+// CARA CLOSE KE 2
+
+function close(nomor) {
+  window.location.reload();
+  }
+
+function getDetail(nomor) {
+  console.log('produk: ', nomor);
+  newBox.style.display = "block"
+  wrapper.style.display = "none"
+  sale.style.display = "none"
+  cta.style.display = "none"
+
+if (nomor === 1) {
+  productBox1.style.display = "flex"
+} else if (nomor === 2) {
+  productBox2.style.display = "flex"
+} else if (nomor === 3) {
+  productBox3.style.display = "flex"
+} else if (nomor === 4) {
+  productBox4.style.display = "flex"
+} else if (nomor === 5) {
+  productBox5.style.display = "flex"
+} else {
+  productBox6.style.display = "flex"
+}
+
+}
+
+
+const menuToggle = document.querySelector('.menu-toggle input');
+const nav = document.querySelector('nav ul');
+const modelViewer = document.querySelector('model-viewer');
+
+menuToggle.addEventListener('click', function() {
+    nav.classList.toggle('slide');
+    modelViewer.classList.toggle('unactive');
+});
+
+
